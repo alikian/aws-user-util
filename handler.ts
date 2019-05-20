d1e24ad9bd26ae0e79dac61d361457cff9b9a2fd
Ali Kian <ali.kian@teradata.com>
2019-05-20 13:16:25 -0700

    read user info

--- handler.ts
+++ handler.ts
@@ -1,12 +1,28 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import * as AWS from 'aws-sdk';
 
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+export const hello: APIGatewayProxyHandler = (event, context, callback) => {
+  console.log(context);           // successful response
+  var params = {
+    UserName: "ali.kian"
+   };
+   var iam = new AWS.IAM();
+   iam.listUserTags(params, function(err, data) {
+     if (err) console.log(err, err.stack); // an error occurred
+     else {
+      console.log(data);           // successful response
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+          input: event,
+        }, null, 2),
+      };
+      callback(null, response);
+     }  
+     
+   });
+
+   
 }
