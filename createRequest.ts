59916844ce9ee54523b210d7b90f367885d4ad90
ali.kian@gmail.com <RyanKian!1>
2019-05-21 22:05:51 -0700

    added dynamodb

--- createRequest.ts
+++ createRequest.ts
@@ -2,8 +2,7 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 import * as AWS from 'aws-sdk';
 import { v4 as uuid } from 'uuid';
-
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+import dynamodb from './dynamodb';
 
 export const handler: APIGatewayProxyHandler = (event, context, callback) => {
   console.log(context);           // successful response
@@ -11,6 +10,8 @@ export const handler: APIGatewayProxyHandler = (event, context, callback) => {
   var params = {
     UserName: userName
   };
+  console.log(event);           // successful response
+
   var iam = new AWS.IAM();
   iam.listUserTags(params, function (err, data) {
     if (err) console.log(err, err.stack); // an error occurred
@@ -28,11 +29,11 @@ export const handler: APIGatewayProxyHandler = (event, context, callback) => {
           email: emailTag.Value,
           createdAt: timestamp,
           userName: userName,
-          updatedAt: null,
-        },
+          updatedAt: null
+        }
       };
 
-      dynamoDb.put(params, (error) => {
+      dynamodb.put(params, (error) => {
         // handle potential errors
         if (error) {
           console.error(error);
@@ -51,14 +52,6 @@ export const handler: APIGatewayProxyHandler = (event, context, callback) => {
         callback(null, response);
       });
 
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-          input: event,
-        }, null, 2),
-      };
-      callback(null, response);
     }
 
   });
